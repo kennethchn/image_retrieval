3c77ee8f89579b704cc6faa6138652f4c5faf407
kenneth <chenyunkang@outlook.com>
2018-09-30 17:34:09 +0800

    20180930

---
+++ Search/base_search.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from __future__ import print_function
+import os
+import sys
+import copy
+sys.path.append('/root/caffe/python')
+sys.path.append('/home/kenneth/ckwork/caffe-master/python')
+import time
+import cv2
+import pickle
+from sklearn.externals import joblib
+import caffe
+import numpy as np
+import ConfigParser
+from IOoperate import rwOperate
+from VLAD import VlADlib
+from FeatureExtract import feature_extract
+
+config = ConfigParser.ConfigParser()
+current_path = os.path.dirname(os.path.abspath(__file__))
+config.read(os.path.join(current_path,'search_config.ini'))
+
+caffe.set_mode_gpu()
+caffe.set_device(0)
+
+def load_net():
+	weight = config.get('CNNParam', 'weight')
+	deploy_file = config.get('CNNParam', 'deploy_file')
+
+	net = caffe.Net( deploy_file, weight, caffe.TEST )
+	return net
+
+def one_extract_cnn_feature(net, image_path):
+	#抽取一张图像的特征
+	transformer = caffe.io.Transformer({'data':net.blobs['data'].data.shape})
+	transformer.set_transpose('data', (2,0,1))
+	transformer.set_raw_scale('data', 255)
+	transformer.set_channel_swap('data', (2,1,0))
+
+	net.blobs['data'].reshape(1,3,224,224)
+	
+	try:
+		simage = caffe.io.load_image( image_path )
+	except:
+		print('read image error, error path:', image_path)
+		return -1
+
+	transformered_image = transformer.preprocess( 'data', simage )
+	net.blobs['data'].data[...] = transformered_image 
+	st0 = time.time()
+	net.forward()
+	print( time.time() - st0 )
+	one_feature = np.squeeze( net.blobs['pool5'].data)
+	return one_feature 
+
+def extract_cnn_feature( image_path):
+	#抽取文件夹下所有图像的特征 
+	net = load_net()
+	transformer = caffe.io.Transformer({'data':net.blobs['data'].data.shape})
+	transformer.set_transpose('data', (2,0,1))
+	transformer.set_raw_scale('data', 255)
+	transformer.set_channel_swap('data', (2,1,0))
+
+	net.blobs['data'].reshape(1,3,224,224)
+	
+	features_dict = {} 
+	image_list = os.listdir(image_path)
+	for img in image_list:
+		image_full_path = os.path.join( image_path, img )
+		try:
+			image = caffe.io.load_image( image_full_path )
+		except:
+			print('read image error, error path:', image_full_path)
+			continue
+		transformered_image = transformer.preprocess( 'data', image )
+		net.blobs['data'].data[...] = transformered_image 
+		st0 = time.time()
+		net.forward()
+		print('extract_feature time:', time.time() - st0 )
+		features_dict[img] = copy.deepcopy( np.squeeze( net.blobs['pool5'].data))
+	return features_dict 
+
+def load_feature( feature_proto_path ):
+    #从存储文件中提取vlad特征或这cnn特征
+	fd = rwOperate.read_dict_des(feature_proto_path )
+	feature_list = []
+	for key in fd.keys():
+		feature_list.append( fd[key] )
+	all_feature = np.array( feature_list, dtype=np.float32 )
+	return fd.keys(), all_feature
+
+def one_extract_vlad_feature( image_path, surf = None):
+    if not surf:
+        surf = feature_extract.create_detector('surf')
+    try:
+        img = cv2.imread(image_path)
+    except:
+        print('errormessage: one_extract_vlad_feature read image wrong!')
+        return -1
+    _, des = feature_extract.detect(surf, img)
+    surf_cluster_path = config.get('UTIL', 'surf_cluster_path')
+    surf_cluster = joblib.load(surf_cluster_path)
+    v = VlADlib.VLAD( des, surf_cluster)
+    v = v[np.newaxis,:].astype(np.float32)
+    return v
+
+def match_creater():
+	#define the matcher function 
+	matcher = cv2.BFMatcher()
+	return matcher
+
+def search(feature, feature_train, matcher = None):
+	st_time = time.time()
+	if not matcher:
+		matcher = cv2.BFMatcher()
+	res = matcher.knnMatch( feature, feature_train, k = 10 )
+	print( 'search time:', time.time() - st_time )
+	return res 
+
+
--- test/search_demo.py
+++ test/search_demo.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
+import os
 import copy
 import numpy as np
 import sys
 sys.path.append('..')
-from Search import cnn_search
+from Search import base_search
 from IOoperate import rwOperate
 from FeatureExtract import cnn_feature_extract as cfe
 def demo1():
 	image_path = './test/com'
 	image_path = './image'
-	net = cnn_search.load_net()
+	net = base_search.load_net()
 	feature_dict = cfe.extract_feature(net, image_path )	
 	rwOperate.save_dict_des( feature_dict, 'image_cnn_dict.feature')
 
 def demo2():
-	fd = rwOperate.read_dict_des( 'image_cnn_dict.feature')
+#	fd = rwOperate.read_dict_des( os.path.join('/home/kenneth/gitstore/datafolder/com', 'image_cnn_dict.feature') )
+	fd = rwOperate.read_dict_des( os.path.join('/home/kenneth/gitstore/datafolder/com', 'descriptors_dict.vlad') )
 	print( fd.keys() )
 	for key in fd.keys():
 		print( fd[key][0] )
@@ -23,15 +25,16 @@ def demo2():
 		print( type( fd[key]))	
 
 def demo3():
-	net = cnn_search.load_net()
+	#cnn search
+	net = base_search.load_net()
 	image_path = '../datafolder/demo.jpg'
-	afeature = cnn_search.one_extract_feature(net, image_path)
+	afeature = base_search.one_extract_cnn_feature(net, image_path)
 	afeature = afeature[np.newaxis, :]		
 	print( afeature.shape, afeature.dtype )
 	
-	name_list, feature_train = cnn_search.load_feature('../datafolder/com/image_cnn_dict.feature')
+	name_list, feature_train = base_search.load_feature('../datafolder/com/image_cnn_dict.feature')
 	print(feature_train.shape, feature_train.dtype )
-	match_result = cnn_search.search_demo( afeature, feature_train )
+	match_result = base_search.search( afeature, feature_train )
 	for mr in match_result:
 		print( name_list[mr[0].trainIdx])
 	
@@ -39,8 +42,8 @@ def demo4():
 #	image_path = '/root/caffework/TestLabelImage'
 #	image_path = './image'
 	image_path = '/home/kenneth/gitstore/datafolder/original_wine_images'
-	search_feature = cnn_search.extract_feature( image_path )
-	name_list, feature_train = cnn_search.load_feature('/home/kenneth/gitstore/datafolder/com/image_cnn_dict.feature')
+	search_feature = base_search.extract_cnn_feature( image_path )
+	name_list, feature_train = base_search.load_feature('/home/kenneth/gitstore/datafolder/com/image_cnn_dict.feature')
 	
 	print( name_list[0:3])
 	print( feature_train[0:3])	
@@ -50,7 +53,7 @@ def demo4():
 		afeature = search_feature[key]
 		afeature = afeature[np.newaxis, :]
 #		print( afeature )
-		match_result = cnn_search.search_demo( afeature, feature_train )
+		match_result = base_search.search( afeature, feature_train )
 
 		temp_record = []
 		temp_record.append( key )
@@ -59,7 +62,18 @@ def demo4():
 		record_result.append( copy.deepcopy(temp_record) )	
 #	print( record_result )
 	np.save( '../../datafolder/record.npy', record_result )
+def demo5():
+	v = base_search.one_extract_vlad_feature('/home/kenneth/gitstore/datafolder/demo.jpg')
+	print( v )
+
+def demo6():
+	#vlad search
+	one_feature = base_search.one_extract_vlad_feature('/home/kenneth/gitstore/datafolder/demo.jpg')
+	name_list, feature_train = base_search.load_feature( os.path.join('/home/kenneth/gitstore/datafolder/com', 'descriptors_dict.vlad'))
+	match_result = base_search.search(one_feature, feature_train)
+	for mr in match_result:
+		print( name_list[mr[0].trainIdx])
 
 
 if __name__ == '__main__':
-    demo4()
+    demo6()
